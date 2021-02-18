94deaf722e936a116d284b799a402f8cea1fcdce
Pion4r <staryliss.nikita.2004@gmail.com>
2021-02-18 13:02:35 +0500

    Враги
    - Добавлены враги.

--- index.js
+++ index.js
@@ -4,11 +4,47 @@ var gamestarted = false;
 
 var friction = 0.08;
 
+var game = {
+    timerSpawnEnemy: null,
+    spawnEnemy: function(){
+        enemy.new();
+    }
+}
+
+var enemy = {
+    list: [],
+    new: function(c = null){
+        let countEnemy = c;
+        if(c == null) countEnemy = randomInteger(0, 4);
+        let sizeZone = Math.round(canvas.width/countEnemy);
+        for(let e = 0; e < countEnemy; e++){
+            let h = randomInteger(10, 30);
+            let color = Math.floor(Math.random()*16777215).toString(16);
+            this.list.push({x: randomInteger(sizeZone*e, (sizeZone*e)+sizeZone), y: -h, w: randomInteger(10, 30), h: h, speed: parseFloat(Math.random().toFixed(2)), color: color});
+        }
+    },
+    update: function(){
+        for(let it of this.list){
+            it.y += it.speed;
+            if(it.y+it.h > canvas.height){
+                this.list.splice(this.list.indexOf(it), 1);
+                this.new(1);
+            }
+        }
+    },
+    render: function(){
+        for(let it of this.list){
+            ctx.fillStyle = "#"+it.color;
+            ctx.fillRect(it.x, it.y, it.w, it.h);
+        }
+    }
+}
+
 var player = {
     speed: 0.06,
     maxspeed: 1,
     x: 0,
-    y: 0,
+    y: canvas.height - (canvas.height / 3),
     velocityx: 0,
     velocityy: 0,
     states: {left: false, right: false},
@@ -62,11 +98,18 @@ var player = {
 }
 
 function ready() {
+    startGame();
+}
+
+function startGame(){
     gamestarted = true;
+    enemy.new();
+    game.timerSpawnEnemy = setInterval(game.spawnEnemy, 2000);
     update();
 }
 
 function update(){
+    enemy.update();
     player.update();
     render();
     if(gamestarted)
@@ -80,6 +123,8 @@ function render(){
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     // Отрисовка персонажа
     player.render();
+    // Отрисовка врагов
+    enemy.render();
 }
 
 function onKey(e){
@@ -99,7 +144,20 @@ function onKey(e){
                 player.states.left = false;
             }
             break;
+        case "ArrowUp":
+            if(e.type == "keydown"){
+                player.states.shot = true;
+            } else {
+                player.states.shot = false;
+            }
+            break;
     }
+}
+
+function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
 }
 
 document.addEventListener("readystatechange", ready);
